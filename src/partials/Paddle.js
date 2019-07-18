0b12c1ea11f94533b303402785705e5d8b9c2bce
Kanika Wadhawan <wadhawan.kanika@gmail.com>
2019-07-18 15:06:40 -0700

    add paddle movement and bounds

--- src/partials/Paddle.js
+++ src/partials/Paddle.js
@@ -14,16 +14,28 @@ export default class Paddle {
       document.addEventListener("keydown", event => {
         switch (event.key) {
             case up:
-              console.log("up");
+              this.up();
+            
               break;
             case down:
-              console.log("down");
+              this.down();
               break;
           }
       });
 
 
     }// end of constructor
+    
+     up(){
+      //this.y = this.y - this.speed;
+      this.y = Math.max(0, this.y-this.speed); // -1 is <0 so max value is always 0.
+     }
+
+     down(){
+        //this.y = this.y + this.speed;
+        this.y = Math.min(this.boardHeight -this.height,this.y + this.speed);
+     }
+
     render(svg){
         let rect = document.createElementNS(SVG_NS, 'rect');
 
